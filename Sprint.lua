7f43cd0183b2c8684e06fef7126317bae584da3d
Zixsoed <rjjdksksosos@gmail.com>
2024-11-04 21:44:49 +0700

    Create Sprint.lua

---
+++ Sprint.lua
@@ -0,0 +1,67 @@
+local NormalWalkSpeed = 16
+repeat task.wait() until game.Players.LocalPlayer.Character.Humanoid.WalkSpeed == 13 or 8.5
+NormalWalkSpeed = 19
+if workspace:FindFirstChild("SeekMovingNewClone") then
+NormalWalkSpeed = 20.5
+return
+end
+local SprintSpeed = 19
+repeat task.wait() until game.Players.LocalPlayer.Character.Humanoid.WalkSpeed == 13 or 8.5
+SprintSpeed = 19
+if workspace:FindFirstChild("SeekMovingNewClone") then
+SprintSpeed = 21
+return
+end
+local CameraEffect = true
+ 
+local cas = game:GetService("ContextActionService")
+local Leftc = Enum.KeyCode.LeftControl
+local RightC = Enum.KeyCode.RightControl
+local player = game:GetService("Players").LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local Humanoid = char:WaitForChild("Humanoid")
+ 
+local Camera = game.Workspace.CurrentCamera
+local TweenService = game:GetService("TweenService")
+local UIS = game:GetService("UserInputService")
+ 
+UIS.InputBegan:Connect(function(key, gameProcessed)
+	if gameProcessed then return end
+	if key.KeyCode == Enum.KeyCode.LeftShift then
+		if CameraEffect == true then
+			TweenService:Create(Camera, TweenInfo.new(0.5), {FieldOfView = 70}):Play()
+		end
+		Humanoid.WalkSpeed = SprintSpeed
+	end
+end)
+ 
+UIS.InputEnded:Connect(function(key, gameProcessed)
+	if gameProcessed then return end
+	if key.KeyCode == Enum.KeyCode.LeftShift then
+		if CameraEffect == true then
+			TweenService:Create(Camera, TweenInfo.new(0.5), {FieldOfView = 70}):Play()
+		end
+		Humanoid.WalkSpeed = NormalWalkSpeed
+	end
+end)
+ 
+--------------------------------------------------- Mobile Button
+ 
+local function handleContext(name, state, input)
+	if state == Enum.UserInputState.Begin then
+		if CameraEffect == true then
+			TweenService:Create(Camera, TweenInfo.new(0.5), {FieldOfView = 70}):Play()
+		end
+		Humanoid.WalkSpeed = SprintSpeed
+	else
+		if CameraEffect == true then
+			TweenService:Create(Camera, TweenInfo.new(0.5), {FieldOfView = 70}):Play()
+		end
+		Humanoid.WalkSpeed = NormalWalkSpeed
+	end
+end
+ 
+cas:BindAction("Sprint", handleContext, true, Leftc, RightC)
+cas:SetPosition("Sprint", UDim2.new(.30, 0, .40, 0))
+cas:SetTitle("Sprint", "Sprint")
+cas:GetButton("Sprint").Size = UDim2.new(.3, 0, .3, 0)
